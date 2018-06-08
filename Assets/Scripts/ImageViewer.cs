059bd90d804a3d54e81c65db54c1eb89e0d647be
Smidish <s.hinterreiter@gmx.de>
2018-06-08 17:47:00 +0200

    init

---
+++ Assets/Scripts/ImageViewer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour {
+
+    public MultiSourceManager mMultiSource;
+    public RawImage mRawImage;
+	
+	// Update is called once per frame
+	void Update () {
+        mRawImage.texture = mMultiSource.GetColorTexture();
+	}
+}
