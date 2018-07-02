0af62ee343649733b9679f91c6511ab1a991c37c
Smidish <s.hinterreiter@gmx.de>
2018-07-02 15:45:38 +0200

    start Scene done

---
+++ Assets/VideoScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoScript : MonoBehaviour {
+    public VideoPlayer VP;
+    public Canvas UICanvas;
+
+    public VideoClip video1;
+    public VideoClip video2;
+
+    void Start () {
+        UICanvas.enabled = false;
+        VP.clip = video1;
+        VP.Play();
+        StartCoroutine(wait(7));
+    }
+
+	public void onButtonClick()
+    {
+        VP.clip = video2;
+        VP.Play();
+        StartCoroutine(wait(3));
+    }
+
+    IEnumerator wait(int s)
+    {
+        if (s == 3)
+        {
+            UICanvas.enabled = false;
+        }
+        yield return new WaitForSeconds(s);
+        if (s == 7)
+        {    
+            UICanvas.enabled = true;
+        }
+        if(s == 3)
+        {
+            SceneChange.ChangeScene("main");
+        }
+    }
+}
--- Assets/_Scripts/UtilitySmi/SceneChange.cs
+++ Assets/_Scripts/UtilitySmi/SceneChange.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour {
 
-    public void ChangeScene(string sceneName)
+    public static void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
